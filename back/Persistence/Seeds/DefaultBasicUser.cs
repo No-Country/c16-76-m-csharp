51f4dafc4f4ff8b99e20307807c189e6c9bce5a4
luisjavierluna <javierlunaluis@gmail.com>
2024-02-13 14:45:33 -0600

    Insert Seed Data and add Script

---
+++ back/Persistence/Seeds/DefaultBasicUser.cs
@@ -0,0 +1,36 @@
+﻿using back.Enums;
+using EmpleaGestion.Entities.User;
+using Microsoft.AspNetCore.Identity;
+
+namespace back.Persistence.Seeds
+{
+    public class DefaultBasicUser
+    {
+        public static async Task SeedAsync(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                // Seed Default Admin User
+                var defaultUser = new AppUser
+                {
+                    UserName = $"user{i}Basic",
+                    Email = $"userBasic{i}@mail.com",
+                    FirstName = $"Neme{i}",
+                    LastName = $"LasName{i}",
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true
+                };
+
+                if (userManager.Users.All(u => u.Id != defaultUser.Id))
+                {
+                    var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                    if (user == null)
+                    {
+                        await userManager.CreateAsync(defaultUser, "123Pa$word");
+                        await userManager.AddToRoleAsync(defaultUser, Roles.Employee.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
--- back/Program.cs
+++ back/Program.cs
@@ -1,6 +1,9 @@
 using back.Interfaces;
 using back.Persistence;
+using back.Persistence.Seeds;
 using back.Services;
+using EmpleaGestion.Entities.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,10 @@ builder.Services.AddTransient<IDateTimeService, DateTimeService>();
 
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -32,4 +39,25 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+try
+{
+    await SeedUsers();
+
+    app.Run();
+}
+catch (Exception)
+{
+
+	throw;
+}
+
+async Task SeedUsers()
+{
+    using var scope = app.Services.CreateScope();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    await DefaultRoles.SeedAsync(userManager, roleManager);
+    await DefaultAdminUser.SeedAsync(userManager, roleManager);
+    await DefaultBasicUser.SeedAsync(userManager, roleManager);
+}
