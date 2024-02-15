fffef70fe22faf663ff18500c7e0346fbfe15727
NahuelA <nahuelarrascaeta22@gmail.com>
2024-02-15 03:56:42 -0300

    Add UserService

--- back/Program.cs
+++ back/Program.cs
@@ -2,7 +2,7 @@ using back.Interfaces;
 using back.Persistence;
 using back.Persistence.Seeds;
 using back.Services;
-using EmpleaGestion.Entities.User;
+using back.Entities.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<IDateTimeService, DateTimeService>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
