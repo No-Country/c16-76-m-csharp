89daf3f8969b87f7dd2d0ab729c207bf7e9dc223
NahuelA <nahuelarrascaeta22@gmail.com>
2024-02-16 01:40:51 -0300

    Add more fields

--- back/DTOs/UserDto.cs
+++ back/DTOs/UserDto.cs
@@ -4,11 +4,12 @@ using Microsoft.AspNetCore.Identity;
 
 namespace back.DTOs;
 
-public class UserDto : BaseEntity
+public class UserDto
 {
+    public string UserName { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Password { get; set; }
+    public string PasswordHash { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
 
