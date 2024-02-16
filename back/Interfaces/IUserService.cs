477aa017689b4578eb3e9855c294360d139c6030
NahuelA <nahuelarrascaeta22@gmail.com>
2024-02-16 01:43:01 -0300

    Change datatypes to return

--- back/Interfaces/IUserService.cs
+++ back/Interfaces/IUserService.cs
@@ -1,13 +1,15 @@
 using back.DTOs;
 using back.Entities.User;
+using back.Utilities.Base;
 
 namespace back.Interfaces;
 
 public interface IUserService
 {
-    public Task<bool> Create(UserDto userDto);
-    public Task<IEnumerable<AppUser>> GetAll(int limit);
-    public Task<AppUser> GetById(string id);
-    public Task<bool> Update(string id, UserDto userDto);
-    public Task<bool> Delete(string id);
+    public Task<BaseResponse<bool>> Create(UserDto userDto);
+    // public Task<BaseResponse<AppUser>> GetAll(int limit);
+    public Task<BaseResponse<AppUser>> GetAll(int limit);
+    public Task<BaseResponse<AppUser>> GetById(string id);
+    public Task<BaseResponse<bool>> Update(string id, UserDto userDto);
+    public Task<BaseResponse<bool>> Delete(string id);
 }
