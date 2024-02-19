a717dffbf39291589f65602bd1924741bdb1fb7b
Gerson Espinoza Sobarzo <gespinoza.sobarzo@gmail.com>
2024-02-18 21:44:10 -0500

    Add slidernav for user panel

---
+++ front/src/app/containers/default-layout/_nav.ts
@@ -0,0 +1,25 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Información',
+    url: '/dashboard',
+    iconComponent: { name: 'cil-speedometer' },
+    badge: {
+      color: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    name: 'Solicitudes',
+    url: '/theme/colors',
+    iconComponent: { name: 'cil-drop' }
+  },
+  {
+    name: 'Asistencia',
+    url: '/theme/typography',
+    linkProps: { fragment: 'someAnchor' },
+    iconComponent: { name: 'cil-pencil' }
+  },
+
+];
