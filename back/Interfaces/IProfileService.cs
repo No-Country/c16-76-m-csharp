95c95f5836c8704a9d0d96e926d2ad6841e554e3
trinidadbenegas <trinidadbenegas.estudio@gmail.com>
2024-02-21 17:19:38 -0300

    Added Profile Interface, Service and Controller

---
+++ back/Interfaces/IProfileService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using back.Entities;
+
+namespace back.Interfaces
+{
+    public interface IProfileService
+    {
+       public Task AddProfile(UserProfile profile);
+    }
+}
---
+++ back/Services/ProfileService.cs
@@ -0,0 +1,26 @@
+﻿using back.Entities;
+using back.Interfaces;
+using back.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace back.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly AppDbContext _context;
+
+        public ProfileService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddProfile(UserProfile profile)
+        {
+
+            await _context.Profiles.AddAsync(profile);
+            await _context.SaveChangesAsync();
+
+        }
+            
+    }
+}
+
