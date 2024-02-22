40eafd4c31fe4b585fb4fa3f5e36f08c14fe01da
Gerson Espinoza Sobarzo <gespinoza.sobarzo@gmail.com>
2024-02-21 23:50:22 -0500

    Add real time clock

--- front/src/app/views/dash-board-employes/dash-board-employes.component.ts
+++ front/src/app/views/dash-board-employes/dash-board-employes.component.ts
@@ -25,6 +25,25 @@ import {
   styleUrl: './dash-board-employes.component.scss',
 })
 export class DashBoardEmployesComponent implements OnInit {
+  currentTime: string = '';
+
   constructor() {}
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    // Actualizar la hora cada segundo
+    setInterval(() => {
+      const now = new Date();
+      this.currentTime = this.formatTime(now.getHours(), now.getMinutes(), now.getSeconds());
+    }, 1000);
+  }
+
+  // Función para formatear la hora en HH:mm:ss
+  private formatTime(hours: number, minutes: number, seconds: number): string {
+    return `${this.padZero(hours)}:${this.padZero(minutes)}:${this.padZero(seconds)}`;
+  }
+
+  // Función para añadir cero delante de un número si es necesario
+  private padZero(num: number): string {
+    return num < 10 ? '0' + num : num.toString();
+  }
 }
