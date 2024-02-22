f80c0f2aa480ef2984696ddbb0ae82c8e2a450e6
NahuelA <nahuelarrascaeta22@gmail.com>
2024-02-22 01:10:54 -0300

    Add assignment validations

---
+++ back/Validations/AssignmentDtoValidator.cs
@@ -0,0 +1,46 @@
+using back.Entities;
+using back.Enums;
+using FluentValidation;
+
+namespace back.Validations
+{
+    public class AssignmentDtoValidator : AbstractValidator<AssignmentDto>
+    {
+        public AssignmentDtoValidator()
+        {
+            RuleFor(x => x.ProfileEmail)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The {PropertyName} field is required.")
+                .EmailAddress()
+                .WithMessage("The {PropertyName} must be a valid email.");
+
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The {PropertyName} field is required.");
+
+            RuleFor(x => x.Description)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The {PropertyName} field is required.");
+
+            RuleFor(x => x.StartDate)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The {PropertyName} field is required.");
+
+            RuleFor(x => x.EndDate)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The {PropertyName} field is required.");
+
+            RuleFor(x => x.Status)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The {PropertyName} field is required.")
+                .When(x => x.Status is Status status)
+                .WithMessage("The {PropertyName} must be : 'ToDo', 'InProgress', 'InReview' or 'Done'.");
+        }
+    }
+}
