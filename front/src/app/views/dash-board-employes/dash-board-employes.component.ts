588757d4d4826b5f2e403bd63bcd689fcd8ac95c
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-02-22 08:31:43 -0600

    add eventListener btn start and finish

--- front/src/app/views/dash-board-employes/dash-board-employes.component.ts
+++ front/src/app/views/dash-board-employes/dash-board-employes.component.ts
@@ -8,6 +8,8 @@ import {
   ButtonModule,
   FormModule,
 } from '@coreui/angular';
+import { format } from '@formkit/tempo';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-dash-board-employes',
@@ -20,30 +22,50 @@ import {
     SpinnerComponent,
     FormModule,
     ButtonModule,
+    CommonModule,
   ],
   templateUrl: './dash-board-employes.component.html',
   styleUrl: './dash-board-employes.component.scss',
 })
 export class DashBoardEmployesComponent implements OnInit {
+  isRunning: boolean = false;
+  startTime: Date | null = null;
   currentTime: string = '';
+  timerInterval: any; // Variable para almacenar el intervalo de tiempo
+  isTimerFinished: boolean = false; // Variable para controlar si el temporizador ha finalizado
+  isActivityStarted: boolean = false; // Variable para controlar si la actividad ha comenzado
 
   constructor() {}
 
-  ngOnInit(): void {
-    // Actualizar la hora cada segundo
-    setInterval(() => {
-      const now = new Date();
-      this.currentTime = this.formatTime(now.getHours(), now.getMinutes(), now.getSeconds());
-    }, 1000);
+  ngOnInit(): void {}
+
+  startTimer() {
+    if (!this.isRunning) {
+      this.startTime = new Date();
+      this.isRunning = true;
+      this.isTimerFinished = false; // Restablecer la variable cuando se inicia el temporizador
+      // Actualizar la hora cada segundo
+      this.timerInterval = setInterval(() => {
+        const now = new Date();
+        this.currentTime = this.formatTime(now, 'es');
+        this.isActivityStarted = true;
+      }, 1000);
+    } else {
+      // Pausar el temporizador
+      clearInterval(this.timerInterval); // Limpiar el intervalo de tiempo
+      this.isRunning = false;
+    }
   }
 
-  // Función para formatear la hora en HH:mm:ss
-  private formatTime(hours: number, minutes: number, seconds: number): string {
-    return `${this.padZero(hours)}:${this.padZero(minutes)}:${this.padZero(seconds)}`;
+  finishTimer() {
+    // Finalizar el temporizador
+    clearInterval(this.timerInterval); // Limpiar el intervalo de tiempo
+    this.isRunning = false;
+    this.startTime = null;
+    this.isTimerFinished = true; // Establecer la variable en true cuando el temporizador finaliza
   }
 
-  // Función para añadir cero delante de un número si es necesario
-  private padZero(num: number): string {
-    return num < 10 ? '0' + num : num.toString();
+  private formatTime(date: Date, lang: string): string {
+    return format(date, 'h:mm:ss a', lang);
   }
 }
